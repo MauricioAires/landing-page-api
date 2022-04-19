0dd8deef09c92a41522b38cfb578369bdb51b213
Mauricio Aires de Freitas <mauricioaires015@gmail.com>
2022-04-19 12:04:48 -0300

    first commit

---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,5 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || "0230289a-7348-4373-913f-85e15a41de47",
+  ADMIN_JWT_SECRET:
+    process.env.JWT_SECRET || "0230289a-7348-4373-913f-85e15a41de47",
+};
